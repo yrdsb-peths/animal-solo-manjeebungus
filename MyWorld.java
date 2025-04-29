96aab51312cb32dcf7bf9d5cbbb690ec1ccdffda
Kelton <keltonkuan@gmail.com>
2025-04-29 14:27:22 -0400

    1.8 Import Label class and keep score

--- MyWorld.java
+++ MyWorld.java
@@ -1,13 +1,27 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
+    
     public MyWorld() {
         super(600, 400, 1);
         
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
         createApple();
+    }
+    
+    /**
+     * Increase score
+     */
+    public void increaseScore() {
+        score++;
+        scoreLabel.setValue(score);
     }
     
     /**
