b5e7a36e29da38a4579591ce93adb4ab4d5659d6
Kelton <keltonkuan@gmail.com>
2025-05-06 14:45:41 -0400

    2.5 Animating the Elephant

--- Elephant.java
+++ Elephant.java
@@ -9,10 +9,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
-    GreenfootImage idle = new GreenfootImage("images/elephant_idle/idle0.png");
+    GreenfootImage[] idle = new GreenfootImage[8];
     
     public Elephant() {
-        setImage(idle);
+        for (int i = 0; i < idle.length; i++) {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
+    
+    /**
+     *  Animate the elephant
+     */
+    int imageIndex = 0;
+    public void animateElephant() {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
     }
     
     public void act()
@@ -25,6 +37,9 @@ public class Elephant extends Actor
         
         //Remove apple if elephant eats it
         eat();
+        
+        //Animate the elephant
+        animateElephant();
     }
     
     /**
